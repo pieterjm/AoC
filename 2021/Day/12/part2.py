e9a16e5081979f5d13e6f75f94144dd192eacdff
Pieter Meulenhoff <p.meulenhoff@qbit.nl>
2021-12-12 09:10:42 +0100

    Added solution for day 12

---
+++ 2021/Day/12/part2.py
@@ -0,0 +1,82 @@
+import re
+import sys
+import numpy as np
+
+links = []
+
+def findpath(path):
+
+    solutions = 0
+    loc = path[-1]
+    
+    options = []
+    for l in links:
+        if l[0] == loc:
+            option = l[1]
+        elif l[1] == loc:
+            option = l[0]
+        else:
+            continue
+
+        Append = True
+        
+
+        
+        occ = {}
+        for p in path:
+            if p.islower():
+                if p in occ:
+                    occ[p] += 1                
+                else:
+                    occ[p] = 1
+
+        if option == 'start' and 'start' in path:
+            Append = False
+            
+        numtwo = 0
+        for p in occ:
+            if occ[p] > 2:
+                Append = False
+            if occ[p] == 2:
+                numtwo += 1
+        if numtwo > 1:
+            Append = False
+
+        if Append:
+            options.append(option)
+
+    #print("options for {}".format(loc),options)
+    for option in options:
+        path.append(option)
+        if option == 'end':
+            solutions += 1
+        else:
+            solutions += findpath(path)
+        del path[-1]
+        
+    return solutions
+        
+def doit(filename):
+    file = open(filename, 'r')
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        if len(line) == 0:
+            break
+        line = line.rstrip().lstrip()
+        
+        
+        m = re.search('^(start|end|[A-Z]+|[a-z]+)-(start|end|[A-Z]+|[a-z]+)', line)
+        if m:
+            links.append([m.group(1),m.group(2)])
+        else:
+            print("problem in line: {}".format(line))
+            sys.exit(0)
+
+    return findpath(['start'])
+    
+filename = sys.argv[1]
+print(doit(filename))
+
+

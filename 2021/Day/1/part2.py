dfb73a731163d816f5c0198b058adbfb496253c1
Pieter Meulenhoff <pieter.meulenhoff@eurofins-cybersecurity.com>
2022-06-29 13:41:44 +0200

    Removed obsolete stuff

--- 2021/Day/1/part2.py
+++ 2021/Day/1/part2.py
@@ -14,6 +14,6 @@ for i in range(1,alen-2,1):
 	depth = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])	
 	if depth > prev:
 		count = count + 1
-#	print("prev = {}, depth = {}, count = {}".format(prev,depth,count))
+	print("prev = {}, depth = {}, count = {}".format(prev,depth,count))
 		
 print(count)
--- 2021/Day/run.sh
+++
@@ -1,5 +0,0 @@
-day=$1
-python ./$day/part1.py ./$day/input.dat
-python ./$day/part2.py ./$day/input.dat
-
-
